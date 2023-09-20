b16c4da7f88d0d9a1ec67e601e526276c89b3dfc
Emma Wong <emma.wong@rd.group>
2023-09-20 16:22:01 +0800

    refactor: code challenge mini mario remove build folder & add .gitignore

--- code_challenge/mini_mario/mini_mario_00_start/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/emmawong/projects/Flutter-Study-Group/code_challenge/mini_mario/mini_mario_00_start/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Users/emmawong/projects/Flutter-Study-Group/code_challenge/mini_mario/mini_mario_00_start/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
--- code_challenge/mini_mario/mini_mario_00_start/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/emmawong/projects/Flutter-Study-Group/code_challenge/mini_mario/mini_mario_00_start/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
